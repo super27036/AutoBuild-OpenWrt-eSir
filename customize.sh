69be559a8d223a3432b2dec9c89b30742e7e2242
super27035 <superssatellite@gmail.com>
2023-09-10 21:10:35 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,11 +5,15 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
-
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
-rm openwrt/package/lean/luci-theme-argon -rf
+# rm openwrt/package/lean/luci-theme-argon -rf
+
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+# Modify default hosename
+sed -i 's/OpenWrt/SUPERouter/g' openwrt/package/base-files/files/bin/config_generate
+# Password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
